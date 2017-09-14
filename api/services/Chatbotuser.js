98e858f9f474afb53644c0fa2e4be1e830895c5d
pratik <pratik.shah@wohlig.com>
2017-09-14 12:31:55 +0530

    a

--- api/services/Chatbotuser.js
+++ api/services/Chatbotuser.js
@@ -317,33 +317,34 @@ var model = {
                 else
                 {
                     //callback(null,{message:1});
-                    const sendmail = require('sendmail')({
-                        logger: {
-                            debug: console.log,
-                            info: console.info,
-                            warn: console.warn,
-                            error: console.error
-                        },
-                        silent: false,
-                        // dkim: { // Default: False 
-                        //     privateKey: fs.readFileSync('./dkim-private.pem', 'utf8'),
-                        //     keySelector: 'mydomainkey'
-                        // },
-                        // devPort: 1025 // Default: False 
-                        // devHost: 'localhost' // Default: localhost 
-                    })
-                    sendmail({
-                        from: 'rohit.mathur@exponentiadata.com',
-                        to: data.email,
-                        subject: 'Detailed Analysis',
-                        html: m_html,
-                        attachments:attachments1,
-                    }, function(err, reply) {
-                        console.log(err && err.stack);
-                        console.dir(reply);
-                        if(!err)
-                            callback(null,{message:1});
-                    });
+                    // const sendmail = require('sendmail')({
+                    //     logger: {
+                    //         debug: console.log,
+                    //         info: console.info,
+                    //         warn: console.warn,
+                    //         error: console.error
+                    //     },
+                    //     silent: false,
+                    //     // dkim: { // Default: False 
+                    //     //     privateKey: fs.readFileSync('./dkim-private.pem', 'utf8'),
+                    //     //     keySelector: 'mydomainkey'
+                    //     // },
+                    //     // devPort: 1025 // Default: False 
+                    //     // devHost: 'localhost' // Default: localhost 
+                    // })
+                    // sendmail({
+                    //     from: 'rohit.mathur@exponentiadata.com',
+                    //     to: data.email,
+                    //     subject: 'Detailed Analysis',
+                    //     html: m_html,
+                    //     attachments:attachments1,
+                    // }, function(err, reply) {
+                    //     console.log(err && err.stack);
+                    //     console.dir(reply);
+                    //     if(!err)
+                    //         callback(null,{message:1});
+                    // });
+                             callback(null,{message:1});
                 }
             }
         );
